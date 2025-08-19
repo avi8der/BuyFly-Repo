14767aafff703fa3588c5b01a5bb84b3b4807083
avi8der <avi8der@gmail.com>
2025-08-19 15:37:13 -0500

    up

---
+++ Server/src/types/express-multer.d.ts
@@ -0,0 +1,19 @@
+
+// Server/src/types/express-multer.d.ts
+// Purpose: teach TypeScript about multer and req.file / req.files
+
+import 'express';
+import 'multer';
+
+declare global {
+  namespace Express {
+    interface Request {
+      file?: Express.Multer.File;
+      files?:
+        | Express.Multer.File[]
+        | { [fieldname: string]: Express.Multer.File[] };
+    }
+  }
+}
+
+export {};
